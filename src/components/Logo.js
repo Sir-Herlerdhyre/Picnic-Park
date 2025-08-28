9356d18437d83268d0dfaf7573354135549f71f1
Aladire Soliu <soliualadire@gmail.com>
2025-08-28 15:33:48 +0100

    initial commit

---
+++ src/components/Logo.js
@@ -0,0 +1,5 @@
+const Logo = () => {
+  return <h1> ✈️ Picnic Park 🧳</h1>;
+};
+
+export default Logo;
---
+++ src/components/Stats.js
@@ -0,0 +1,24 @@
+const Stats = ({ items }) => {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Let's start adding items to your list 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything packed! Let's go ✈️"
+          : ` 💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
+    </footer>
+  );
+};
+
+export default Stats;
